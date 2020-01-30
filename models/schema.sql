d33e684332da796fd2a5828c0bb3270be7693764
Tucker Fiumara <54918455+thy-turk@users.noreply.github.com>
2020-01-29 20:08:59 -0700

    Updating schema (#8)

--- models/schema.sql
+++ models/schema.sql
@@ -21,3 +21,17 @@ CREATE TABLE recipe (
     total_time INT NOT NULL,
     primary key (id)
 );
+
+CREATE DATABASE recipes_db;
+
+USE recipes_db;
+
+CREATE TABLE users (
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+username VARCHAR(20) NOT NULL,
+password CHAR(60) NOT NULL,
+fullName VARCHAR(60) NOT NULL,
+PRIMARY KEY (id),
+INDEX id_UNIQUE (id ASC),
+INDEX username_UNIQUE (username ASC)
+)
