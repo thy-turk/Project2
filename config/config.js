c08e9747bc53f1d351d82b4787c8a6c6cfc278a4
Amet <ametueraspirant@yahoo.com>
2020-01-29 21:34:37 -0700

    updated the json file and basic api calls

---
+++ config/config.js
@@ -0,0 +1,25 @@
+var creds = require("./pass");
+
+var config = {
+  development: {
+    username: creds.user,
+    password: creds.pass,
+    database: "recipes_db",
+    host: "localhost",
+    dialect: "mysql"
+  },
+  test: {
+    username: creds.user,
+    password: creds.pass,
+    database: "recipes_db",
+    host: "localhost",
+    dialect: "mysql",
+    logging: false
+  },
+  production: {
+    use_env_variable: "JAWSDB_URL",
+    dialect: "mysql"
+  }
+}
+
+module.exports = config;
--- public/js/apicall.js
+++ public/js/apicall.js
@@ -1,11 +1,14 @@
-$("#recipeSearch").on("click", function (event) {
-    var recipeSave = $("#recipeSearch").val().trim();
-    var queryurl1 = "https://api.edamam.com/api/nutrition-data?app_id=7e1b6072&app_key=b2f9db58b673c1dbdf0bbc30928a9d81&ingr=" + recipeSave.split(" ").join("%20");
-    
-    $.ajax({
-        url: queryurl1,
-        method: "GET"
-    }).then(function (event) {
-        console.log(event);
+$(function() {
+    $("#submit").on("click", function (event) {
+        event.preventDefault();
+        var recipeSave = $("#recipeSearch").val().trim();
+        var queryurl1 = "https://api.edamam.com/api/nutrition-data?app_id=7e1b6072&app_key=b2f9db58b673c1dbdf0bbc30928a9d81&ingr=" + recipeSave.split(" ").join("%20");
+        
+        $.ajax({
+            url: queryurl1,
+            method: "GET"
+        }).then(function (event) {
+            console.log(event);
+        });
     });
 });
