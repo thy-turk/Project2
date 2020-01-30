9adabef1ac884f9a6054a407dc51cb77c3e280d1
Mike Smith <hockeydad66@live.com>
2020-01-29 19:55:22 -0700

    update to schema file

--- models/schema.sql
+++ models/schema.sql
@@ -1,21 +1,12 @@
+DROP DATABASE IF EXISTS recipes_db;
 CREATE DATABASE recipes_db;
-
 USE recipes_db;
-
-CREATE TABLE user_profile (
-    id INT NOT NULL AUTO_INCREMENT,
-    username VARCHAR(50) NOT NULL,
-    bmr INT NOT NULL,
-    user_weight INT NOT NULL,
-    user_height INT NOT NULL,
-    primary key (id)
-);
-
-CREATE TABLE recipe (
-    id INT NOT NULL AUTO_INCREMENT,
-    recipe_name VARCHAR(50) NOT NULL,
-    diet VARCHAR(50) NOT NULL,
-    ingredient VARCHAR(50) NOT NULL,
-    total_time INT NOT NULL,
-    primary key (id)
-);
+CREATE TABLE users (
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+username VARCHAR(20) NOT NULL,
+password CHAR(60) NOT NULL,
+fullName VARCHAR(60) NOT NULL,
+PRIMARY KEY (id),
+INDEX id_UNIQUE (id ASC),
+INDEX username_UNIQUE (username ASC)
+)
