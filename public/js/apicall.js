f152515f0fb47ade49612d4fe4f7d52a7f76f5f3
Amet <ametueraspirant@yahoo.com>
2020-02-02 22:17:03 -0700

    expanded saving recipies to the database
    it doesn't work tho

--- public/js/apicall.js
+++ public/js/apicall.js
@@ -35,10 +35,20 @@ $("#submit").on("click", function () {
         console.log("TEST" + $(this).attr("id"));
         $(document).on("click", ".savebtn", function(event) {
             var foodstuffs = results[$(this).attr("id")].recipe;
+            if(dietLabels.length > 0){var diet = foodstuffs.dietLabels.join("~688")}else{var diet = "none"};
+            if(healthLabels.length > 0){var health = foodstuffs.healthLabels.join("~688")}else{var health = "none"};
+            if(cautions.length > 0){var warning = foodstuffs.cautions.join("~688")}else{var warning = "none"};
+            if(ingredientLines.length > 0){var ingredient = foodstuffs.ingredientLines.join("~688")}else{var ingredient = "none"};
             var recipeInfo = {
                 recipeName: foodstuffs.label,
+                recipeImage: foodstuffs.image,
                 recipeurl: foodstuffs.url,
-                recipeImage: foodstuffs.label
+                calories: foodstuffs.calories,
+                dietLabels: diet,
+                healthLabels: health,
+                warningLabels: warning,
+                ingredientLines: ingredient,
+                yield: foodstuffs.yield
             }
             console.log(recipeInfo)
             $.ajax({
@@ -59,6 +69,25 @@ $(document).ready(function () {
             method: "GET"
         }).then(function (response) {
             console.log(response);
+            for (let i = 0; i < response.length; i++) {
+                var recipe = response[i];
+                $("#savedRecipes").append(
+                    "<div class = 'resultbox w-full xl:w-3/4'>" +
+                        "\n<div class = 'flex flex-row justify-between'>" +
+                            "\n<h1 class = 'bg-gray-900 text-white p-5 px-10 text-xl flex-1 shadow'>" + recipe.recipeName + "</h1>" +
+                            "\n<div class = 'flex flex-row'>" +
+                                "\n<a href = '" + recipe.recipeurl + "' target = '_blank' class = 'bg-teal-300 text-white p-5 text-lg shadow'>Cooking Instructions</a>" +
+                                "\n<a href = '#' type = 'submit' id='" + i + "' class = 'savebtn bg-teal-300 text-white p-5 text-lg shadow'>Save for Later</a>" +
+                            "\n</div>" +
+                        "\n</div>" +
+                        "\n<div class = 'flex flex-row bg-white shadow'>" +
+                            "\n<img src = '" + recipe.recipeImage + "' alt = '" + recipe.recipeName + "' class = 'apimage'>" +
+                            "\n<div class = 'nutrition-box'>" +
+                            "\n</div>" +
+                        "\n</div>" +
+                    "\n</div>"
+                );
+            }
         });
     }
 })
